a19b72fef67036515129fa5e00251d7f93a808b1
Daryn Nguyen <116467220+darynng@users.noreply.github.com>
2026-03-15 08:40:36 -0400

    Update ign_bridge.launch.py

--- igt_ignition/launch/ign_bridge.launch.py
+++ igt_ignition/launch/ign_bridge.launch.py
@@ -14,13 +14,13 @@ def generate_launch_description():
 	pkg_ros_ign_gazebo = get_package_share_directory('ros_ign_gazebo')
 	pkg_igt_ignition = get_package_share_directory('igt_ignition')
 
-	namespace = 'igt_one'
+	namespace = ''
 	use_sim_time = LaunchConfiguration('use_sim_time')
-	robot_name = 'igt_one'
+	robot_name = 'RMP'
 	ign_model_prefix = '/model/' + robot_name
 
 	# clock bridge
-	clock_bridge = Node(package='ros_ign_bridge', executable='parameter_bridge',
+	clock_bridge = Node(package='ros_gz_bridge', executable='parameter_bridge',
 			namespace=namespace,
 			name='clock_bridge',
 			output='screen',
@@ -29,7 +29,7 @@ def generate_launch_description():
 			)
 
 	# cmd_vel bridge 
-	cmd_vel_bridge = Node(package='ros_ign_bridge', executable='parameter_bridge',
+	cmd_vel_bridge = Node(package='ros_gz_bridge', executable='parameter_bridge',
 			namespace = namespace,
 			name = 'cmd_vel_bridge',
 			output='screen',
@@ -42,58 +42,9 @@ def generate_launch_description():
 			remappings = [
 				(ign_model_prefix + '/cmd_vel', '/cmd_vel')
 			])
-
-	# odometry bridge 
-	odometry_bridge = Node(package='ros_ign_bridge', executable='parameter_bridge',
-			namespace = namespace,
-			name = 'odometry_bridge',
-			output='screen',
-			parameters=[{
-				'use_sim_time': use_sim_time
-			}],
-			arguments = [
-				 ign_model_prefix + '/odometry' + '@nav_msgs/msg/Odometry' + '[ignition.msgs.Odometry'
-			],
-			remappings = [
-				(ign_model_prefix + '/odometry', '/odom')
-			])
-
-	# joint state bridge 
-	joint_state_bridge = Node(package='ros_ign_bridge', executable='parameter_bridge',
-			namespace = namespace,
-			name = 'joint_state_bridge',
-			output='screen',
-			parameters=[{
-				'use_sim_time': use_sim_time
-			}],
-			arguments = [
-				'/world/default/model/igt_one/joint_state'
-				 + '@sensor_msgs/msg/JointState' + '[ignition.msgs.Model'
-			],
-			remappings = [
-				('/world/default/model/igt_one/joint_state', '/joint_states')
-			])
-
-   """ lidar bridge 
-	lidar_bridge = Node(package='ros_ign_bridge', executable='parameter_bridge',
-			namespace = namespace,
-			name = 'lidar_bridge',
-			output='screen',
-			parameters=[{
-				'use_sim_time': use_sim_time
-			}],
-			arguments = [
-				 ign_model_prefix + '/laserscan' + '@sensor_msgs/msg/LaserScan' + '[ignition.msgs.LaserScan',
-				 ign_model_prefix + '/laserscan/points' + '@sensor_msgs/msg/PointCloud2' + '[ignition.msgs.PointCloudPacked'
-			],
-			remappings = [
-				(ign_model_prefix + '/laserscan/points', '/scan/points'),
-				(ign_model_prefix + '/laserscan', '/scan')
-			])"""
-
-
+			
 	# color camera bridge 
-	color_camera_bridge = Node(package='ros_ign_bridge', executable='parameter_bridge',
+	color_camera_bridge = Node(package='ros_gz_bridge', executable='parameter_bridge',
 			namespace = namespace,
 			name = 'color_camera_bridge',
 			output='screen',
@@ -108,7 +59,9 @@ def generate_launch_description():
 			])
 
 	# depth camera bridge 
-	depth_camera_bridge = Node(package='ros_ign_bridge', executable='parameter_bridge',
+	depth_camera_bridge = Node(
+			package='ros_gz_bridge', 
+			executable='parameter_bridge',
 			namespace = namespace,
 			name = 'depth_camera_bridge',
 			output='screen',
@@ -116,16 +69,56 @@ def generate_launch_description():
 				'use_sim_time': use_sim_time
 			}],
 			arguments = [
-				 ign_model_prefix + '/depth_camera' + '@sensor_msgs/msg/Image' + '[ignition.msgs.Image',
-				 ign_model_prefix + '/depth_camera/points' + '@sensor_msgs/msg/PointCloud2' + '[ignition.msgs.PointCloudPacked'
+				'/depth_camera' + '@sensor_msgs/msg/Image' + '[ignition.msgs.Image',
+				'/depth_camera/points' + '@sensor_msgs/msg/PointCloud2' + '[ignition.msgs.PointCloudPacked',
+				'/camera_info' + '@sensor_msgs/msg/CameraInfo'+ '[ignition.msgs.CameraInfo'
+			])
+			
+	# infrared camera bridge 
+	infrared_camera_bridge = Node(package='ros_gz_bridge', executable='parameter_bridge',
+			namespace = namespace,
+			name = 'infrared_camera_bridge',
+			output='screen',
+			parameters=[{
+				'use_sim_time': use_sim_time
+			}],
+			arguments = [
+				 ign_model_prefix + '/infrared_camera' + '@sensor_msgs/msg/Image' + '[ignition.msgs.Image'
 			],
 			remappings = [
-				(ign_model_prefix + '/depth_camera', '/depth_camera'),
-				(ign_model_prefix + '/depth_camera/points', '/depth_camera/points')
+				(ign_model_prefix + '/infrared_camera', '/infrared_camera')
+			])
+			
+	# IMU bridge 
+	imu_bridge = Node(package='ros_gz_bridge', executable='parameter_bridge',
+			namespace = namespace,
+			name = 'imu_bridge',
+			output='screen',
+			parameters=[{
+				'use_sim_time': use_sim_time
+			}],
+			arguments = [
+				 ign_model_prefix + '/imu' + '@sensor_msgs/msg/Imu' + '[ignition.msgs.IMU'
+			],
+			remappings = [
+				(ign_model_prefix + '/imu', '/imu')
 			])
 	
+	# odom bridge 
+	odom_bridge = Node(package='ros_gz_bridge', executable='parameter_bridge',
+			namespace = namespace,
+			name = 'odometry_bridge',
+			output='screen',
+			parameters=[{
+				'use_sim_time': use_sim_time
+			}],
+			arguments = [
+				 ign_model_prefix + '/odom' + '@nav_msgs/msg/Odometry' + '[ignition.msgs.Odometry'
+			])
+
+			
 	# odom to base_link transform bridge
-	odom_base_tf_bridge = Node(package='ros_ign_bridge', executable='parameter_bridge',
+	odom_base_tf_bridge = Node(package='ros_gz_bridge', executable='parameter_bridge',
 			namespace = namespace,
 			name = 'odom_base_tf_bridge',
 			output = 'screen',
@@ -134,28 +127,30 @@ def generate_launch_description():
 			}],
 			arguments = [
 				ign_model_prefix + '/tf' + '@tf2_msgs/msg/TFMessage' + '[ignition.msgs.Pose_V'
-			],
-			remappings = [
-				(ign_model_prefix + '/tf', '/tf')
+			])
+			
+	# joint states bridge 
+	joint_states_bridge = Node(package='ros_gz_bridge', executable='parameter_bridge',
+			namespace = namespace,
+			name = 'joint_states_bridge',
+			output='screen',
+			parameters=[{'use_sim_time': use_sim_time}],
+			arguments = [
+				'/joint_states' + '@sensor_msgs/msg/JointState' + '[ignition.msgs.Model'
 			])
 
-	"""lidar_stf = Node(package='tf2_ros', executable='static_transform_publisher',
-            namespace = namespace,
-            name = 'lidar_stf',
-                arguments = [
-                    '0', '0', '0', '0', '0', '0', '1',
-                    'lidar',
-                    'igt_one/base_link/front_lidar'
-            ])"""
 
 	return LaunchDescription([
         DeclareLaunchArgument('use_sim_time', default_value=['false'],
                             description='use sim time from /clock'),
-        clock_bridge,
-		cmd_vel_bridge,
-		joint_state_bridge,
-		odometry_bridge,
-		color_camera_bridge,
-		depth_camera_bridge,
-		odom_base_tf_bridge
+			clock_bridge,
+			cmd_vel_bridge,
+			color_camera_bridge,
+			depth_camera_bridge,
+			infrared_camera_bridge,
+			imu_bridge,
+			odom_bridge,
+			odom_base_tf_bridge,
+			joint_states_bridge
 	])
+
